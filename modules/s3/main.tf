5e5aaa467c5a4bb6a20a913950b0a12a031cce07
m-bilal-khan <bilalkhan.192003@gmail.com>
2025-09-18 14:55:46 +0500

    Bug fix

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,34 +1,8 @@
 # -----------------------------
 # S3 bucket
 # -----------------------------
-resource "aws_s3_bucket" "this" {
+data "aws_s3_bucket" "this" {
   bucket = "${var.project_name}-${var.env}-bucket"
-  tags = {
-    Name = "${var.project_name}-${var.env}-bucket"
-    Env  = var.env
-  }
-}
-
-#-----------------------------
-# s3 bucket versioning
-#-----------------------------
-resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.this.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# -----------------------------
-# Server-side encryption
-#-----------------------------
-resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  bucket = aws_s3_bucket.this.id
-  rule {
-    apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-    }
-  }
 }
 
 # -----------------------------
