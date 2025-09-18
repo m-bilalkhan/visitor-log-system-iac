e254b6b901bfcb0cf9b0cc31b25e2758845cdd14
m-bilal-khan <bilalkhan.192003@gmail.com>
2025-09-18 16:47:24 +0500

    Lets go

--- modules/auto-scaling/main.tf
+++ modules/auto-scaling/main.tf
@@ -20,7 +20,7 @@ resource "aws_iam_role" "ec2_role" {
 # ----------------------
 resource "aws_iam_role_policy_attachment" "ssm_access" {
   role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 # ----------------------
