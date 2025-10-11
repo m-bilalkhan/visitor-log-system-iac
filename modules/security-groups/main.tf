d2c88b704694cae39e3457bd9a74236fb5ea53bf
m-bilal-khan <bilalkhan192003@gmail.com>
2025-10-11 17:04:05 +0500

    fix

--- modules/security-groups/main.tf
+++ modules/security-groups/main.tf
@@ -123,11 +123,13 @@ resource "aws_security_group" "lambda_sg" {
   }
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_lambda_all_outbound_egress" {
+resource "aws_vpc_security_group_egress_rule" "allow_lambda_outbound_egress" {
   security_group_id = aws_security_group.lambda_sg.id
-  description = "Allow all outbound traffic"
-  ip_protocol       = "-1"        # All protocols
-  cidr_ipv4         = "0.0.0.0/0" # Allow all outbound traffic
+  description = "Allow outbound traffic"
+  from_port   = 443
+  to_port     = 443
+  ip_protocol = "tcp"
+  cidr_ipv4   = "0.0.0.0/0" # Allow all outbound traffic
   tags = {
     Name        = "${var.project_name}-${var.env}-lambda-sg-egress"
     Env = var.env
