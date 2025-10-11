8265dd87f07f09c5f280eae1040c1780f6b8b5a8
m-bilal-khan <bilalkhan192003@gmail.com>
2025-10-11 20:15:23 +0500

    bug fix

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -119,7 +119,7 @@ resource "aws_lambda_layer_version" "psycopg2" {
 }
 
 resource "aws_lambda_function" "bootstrap" {
-  filename         = data.archive_file.lambda_zip
+  filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   function_name    = "${var.project_name}-${var.env}-rds-configurer"
   role             = aws_iam_role.lambda_role.arn
